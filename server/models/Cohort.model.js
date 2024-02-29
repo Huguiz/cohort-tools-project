56b2e6ef4b1119cd9f2f58e76c7313805186a385
Huguiz <hugo.maurin@hotmail.com>
2024-02-29 14:46:29 +0100

    Adding options to mongoose shema

--- server/models/Cohort.model.js
+++ server/models/Cohort.model.js
@@ -1,25 +1,58 @@
 // ./models/Cohort.model.js
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
- 
+
 // CREATE SCHEMA
 // Schema - describes and enforces the structure of the documents
 const cohortSchema = new Schema({
-  cohortSlug: String,
-  cohortName: String,
-  program: String,
-  format: String,
-  campus: String,
-  startDate: Date,
-  endDate: Date,
-  inProgress: Boolean,
-  programManager: String,
-  leadTeacher: String,
-  totalHours: Number,
+  cohortSlug: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  cohortName: {
+    type: String,
+    required: true
+  },
+  program: {
+    type: String,
+    enum: ["Web Dev", "UX/UI", "Data Analytics", "Cybersecurity"]
+  },
+  format: {
+    type: String,
+    enum: ["Full Time", "Part Time"]
+  },
+  campus: {
+    type: String,
+    enum: ["Madrid", "Barcelona", "Miami", "Paris", "Berlin", "Amsterdam", "Lisbon", "Remote"]
+  },
+  startDate: {
+    type: Date,
+    default: Date.now
+  },
+  endDate: {
+    type: Date
+  },
+  inProgress: {
+    type: Boolean,
+    default: false
+  },
+  programManager: {
+    type: String,
+    required: true
+  },
+  leadTeacher: {
+    type: String,
+    required: true
+  },
+  totalHours: {
+    type: Number,
+    default: 360
+  },
 });
- 
+
 // CREATE MODEL
 const Cohort = mongoose.model("Cohort", cohortSchema);
- 
+
 // EXPORT THE MODEL
 module.exports = Cohort;
